b8341c3e7a9a7d5acfde9bc0591b5086dfcc6edf
Mary Jenn <mary@maryfjenn.com>
2013-11-19 23:59:42 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EventMapper::Application.config.secret_key_base = 'a69e144fc434dbf3f553101d5182cc45cd222e6a0a2aad434f64129e73708be65eec80451862a7fde933b57d4b9b74f93eb283a8c65ae9ea025a9214635b4f89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EventMapper::Application.config.session_store :cookie_store, key: '_event_mapper_session'
