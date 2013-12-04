f2d37acda7208a31d84e63797153a621127411a8
Mary Jenn <mary@maryfjenn.com>
2013-12-03 19:32:41 -0800

    got query working from queries_controller. Woo

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -4,14 +4,17 @@ class QueriesController < ApplicationController
     def index
       @geolocator = session[:current_position]
       @end_time = (Time.now.to_i + 7200) * 1000
-      @muquery = "https://api.meetup.com/2/open_events?&sign=true&lon=%s&lat=%s&time=0,%s&category=34&radius=1&page=15&key=324f2a59787e80805a38532c2ea772c" % [@geolocator.latitude, @geolocator.longitude, @end_time]
+      @muquery = "https://api.meetup.com/2/open_events?&sign=true&lon=%s&lat=%s&time=0,%s&category=34&radius=1&page=15&key=324f2a59787e80805a38532c2ea772c" % [@geolocator.longitude, @geolocator.latitude, @end_time]
       
       api_uri = URI(@muquery)
-      https = Net::HTTP.new(api_uri.host, api_uri.port)
-      https.use_ssl=true
+      https = Net::HTTP.get_response(URI.parse(@muquery))
+      # https.use_ssl=true
       p api_uri
-      @r = https.request_get(api_uri.path)
-      render :json => @r
+      data = https.body
+      result = JSON.parse(data)
+      p result
+      # @r = https.request_get(api_uri.path)
+      # render :json => @r
   end
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_reader :name, :lat, :lon, :radius, :description, :event_url, :time
+  
+  #@event = Event.new
+  
 end
