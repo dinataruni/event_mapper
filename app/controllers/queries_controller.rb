7cb6b307f4a4ca3379da938448943aae5540736d
Mary Jenn <mary@maryfjenn.com>
2013-12-03 19:40:13 -0800

    fixed conflict

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -4,12 +4,7 @@ class QueriesController < ApplicationController
     def index
       @geolocator = session[:current_position]
       @end_time = (Time.now.to_i + 7200) * 1000
-      @muquery = "https://api.meetup.com/2/open_events?&sign=true&lon=%s&lat=%s&time=0,%s&category=34&radius=1&page=15&key=324f2a59787e80805a38532c2ea772c" % [@geolocator.latitude, @geolocator.longitude, @end_time]
-<<<<<<< HEAD
-=======
-      
-      p @muquery
->>>>>>> 00a87e80cedbc542cdfb4a003218a7b886afda2a
+      @muquery = "https://api.meetup.com/2/open_events?&sign=true&lon=%s&lat=%s&time=0,%s&category=34&radius=1&page=15&key=324f2a59787e80805a38532c2ea772c" % [@geolocator.longitude, @geolocator.latitude, @end_time]
       
       api_uri = URI(@muquery)
       https = Net::HTTP.get_response(URI.parse(@muquery))
